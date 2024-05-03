9483f43f650f1eb8fc94a2408e7484b2f89e0db5
AlanBerrios <112836766+AlanBerrios@users.noreply.github.com>
2024-05-03 15:38:21 -0400

    last update unu

--- tingeso-alan-backend/src/test/java/com/example/tingesoBackendAlan/TingesoBackendAlanApplicationTests.java
+++ tingeso-alan-backend/src/test/java/com/example/tingesoBackendAlan/TingesoBackendAlanApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.tingesoBackendAlan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class TingesoBackendAlanApplicationTests {
 
-	// @Test
+	@Test
 	void contextLoads() {
 	}
 
